8a5562eef822fbc28880c582f692b5c95ba08187
Divya Shakti <166431272+dscoder1@users.noreply.github.com>
2025-07-29 20:32:08 +0530

    Add files via upload

---
+++ SketchImage.py
@@ -0,0 +1,37 @@
+import cv2 
+from tkinter import *
+from tkinter.messagebox import showinfo,showerror
+def savefile():
+    if(pathinput.get()[-1]=='"' or pathinput.get()[0]=='"'):
+        showerror(title="Sketch Generator",message="Give Path Without Quotes")
+    image=cv2.imread(pathinput.get())
+    gray_img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    invert=cv2.bitwise_not(gray_img)
+    blur=cv2.GaussianBlur(invert,(21,21),0)
+    inverted_blur=cv2.bitwise_not(blur)
+    Sketch=cv2.divide(gray_img,inverted_blur,scale=256.0)
+    cv2.imwrite(fileinput.get()+'.png',Sketch)
+    showinfo(title="Sketch Generator",message="File Saved With "+fileinput.get()+".png")
+    pathinput.set("")
+    fileinput.set("")
+    
+page=Tk()
+page.title("Generate Sketch Image ..")
+page.iconbitmap("sketch.ico")
+page.geometry("1500x750+10+10")
+page.resizable(False,False)
+pathinput=StringVar()
+fileinput=StringVar()
+TitleLabel=Label(page,text="Generate Sketch Image",font=("Calibri",30,"bold"),bg="black",relief=SUNKEN,fg="white")
+TitleLabel.place(x=130,y=50,height=70,width=1200)
+LinkLabel=Label(page,text="Image Path: ",font=("Calibri",20))
+LinkLabel.place(x=280,y=200,height=60,width=150)
+FileLabel=Label(page,text="File Name: ",font=("Calibri",20))
+FileLabel.place(x=280,y=300,height=60,width=150)
+LinkInput=Entry(page,textvariable=pathinput,font=("Calibri",20))
+LinkInput.place(x=450,y=200,height=60,width=700)
+FileInput=Entry(page,textvariable=fileinput,font=("Calibri",20))
+FileInput.place(x=450,y=300,height=60,width=700)
+SaveButton=Button(page,text="Save",bg="black",fg="white",font=("Arial Black",20,"bold"),command=savefile)
+SaveButton.place(x=700,y=500,height=70,width=200)
+page.mainloop()
